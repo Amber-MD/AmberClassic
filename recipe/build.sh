2c39f861011579906f531a497c3d6a2a9afc98ba
David A Case <david.case@rutgers.edu>
2024-11-25 16:10:24 -0700

    more conda tweaking

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,7 +1,9 @@
 #!/bin/sh
 
-source ./AmberClassic.sh
-./configure --openmp
+# source ./AmberClassic.sh
+export MKLROOT=/opt/intel/oneapi/mkl/latest
+
+./configure --openmp --mkl
 
 cd src
 make serial
