84e8bd7b48cf925607120bcd6269ae208e889e80
David A Case <david.case@rutgers.edu>
2024-11-30 21:34:34 -0700

    tweaks to testing environment

--- recipe/build.sh
+++ recipe/build.sh
@@ -8,6 +8,6 @@ cd src
 make serial
 cd ..
 
-rsync -av README.md LICENSE AmberClassic.sh dat bin lib test $PREFIX
+rsync -av README.md LICENSE config_testing.h AmberClassic.sh dat bin lib test $PREFIX
 mkdir -p $PREFIX/doc
 rsync -av doc/AmberClassic.pdf $PREFIX/doc
