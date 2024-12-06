a985c9561d0b3b80f482b3ab04868744ec90d1a2
David A Case <david.case@rutgers.edu>
2024-12-06 14:48:59 -0700

    seems to work now with mpich, which emits fewer annoying messages than does openmpi

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,8 +1,10 @@
 #!/bin/sh
 
 ./configure --conda --openmp
-
 (cd src && make conda)
+
+./configure --conda --mpi
+(cd src && make clean && make parallel)
 
 rsync -a README.md LICENSE AmberClassic.sh config_testing.h include dat bin lib $PREFIX
 mkdir -p $PREFIX/doc
