9728c65865ad996f7717eee6577fcde685cbe2fb
David A Case <david.case@rutgers.edu>
2024-12-07 22:49:51 -0700

    add cuda builds to conda build.sh

--- recipe/build.sh
+++ recipe/build.sh
@@ -2,14 +2,19 @@
 
 export CUDA_HOME=/usr/local/cuda
 
-# ./configure --conda --openmp
-# (cd src && make conda)
+./configure --conda --openmp
+(cd src && make conda)
 
-# ./configure --conda --mpi
-# (cd src && make clean && make parallel)
+./configure --conda --mpi --openmp
+(cd src && make clean && make parallel)
 
-./configure --conda --cuda
-(cd src && make clean && make cuda)
+if [ "`uname`" == "Linux" ]; then
+   ./configure --conda --cuda
+   (cd src && make clean && make cuda)
+
+   ./configure --conda --cuda --mpi
+   (cd src && make clean && make mpicuda)
+fi
 
 rsync -a README.md LICENSE AmberClassic.sh config_testing.h include dat bin lib $PREFIX
 mkdir -p $PREFIX/doc
